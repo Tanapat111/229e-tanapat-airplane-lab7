2d84bbba34d2a7bc500b3b4fee49d718eba97d8d
Tanapat111 <tanapat.pala@bumail.net>
2025-03-13 10:05:21 +0700

    script

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-   
+    Rigidbody rb;
+    const float G = 0.00674f;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+   void Attract(Gravity other)
+   {
+        Rigidbody otherRb = other.rb;
+
+        //find distance between two objects
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+       
+        //addforce
+        otherRb.AddForce(finalForce);
+   }
 }
